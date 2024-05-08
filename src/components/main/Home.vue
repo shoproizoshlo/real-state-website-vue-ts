a066141feb7a84da61132da4631a9b90d6f4f56c
shoproizoshlo <iamsusunny@gmail.com>
2024-05-08 16:52:14 +0200

    styled home section

--- src/components/main/Home.vue
+++ src/components/main/Home.vue
@@ -1,49 +1,42 @@
 <template>
   <section class="home section" id="home">
-    <div class="home__container wrapper grid">
+    <div class="pt-16 gap-y-14 wrapper grid">
       <div class="home__data">
-        <h1 class="home__title">
+        <h1 class="text-white mb-5 text-biggest-font-size home__title">
           Discover <br />
           Most Suitable <br />
           Property
         </h1>
-        <p class="home__description">
+        <p class="text-text-color-light mb-8">
           Find a variety of properties that suit you very easily, forget all
           difficulties in finding a residence for you
         </p>
 
-        <form action="" class="home__search">
-          <i class="bx bxs-map"></i>
+        <form
+          action=""
+          class="mb-4 py-1 ps-1 pe-3 bg-body-color flex items-center rounded-xl border-2 border-solid border-text-color-light"
+        >
+          <i class="text-xl text-first-color bx bxs-map"></i>
           <input
             type="search"
             placeholder="Search by location..."
-            class="home__search-input"
+            class="my-0 mx-2 w-11/12 bg-body-color text-text-color home__search-input"
           />
           <button class="button">Search</button>
         </form>
 
-        <div class="home__value">
-          <div>
-            <h1 class="home__value-number">9K <span>+</span></h1>
-            <span class="home__value-description">
-              Premium <br />
-              Product
-            </span>
-          </div>
-          <div>
-            <h1 class="home__value-number">2K <span>+</span></h1>
-            <span class="home__value-description">
-              Happy <br />
-              Customer
-            </span>
-          </div>
-          <div>
-            <h1 class="home__value-number">28K <span>+</span></h1>
-            <span class="home__value-description">
-              Awards <br />
-              Winning
-            </span>
-          </div>
+        <div class="flex gap-x-10">
+          <HomeValue
+            number="9K"
+            description1="Premium"
+            description2="Product"
+          />
+          <HomeValue number="2K" description1="Happy" description2="Customer" />
+          <HomeValue
+            number="28K"
+            description1="Awards"
+            description2="Winning"
+          />
         </div>
       </div>
 
@@ -58,7 +51,23 @@
 </template>
 
 <script>
-export default {};
+import HomeValue from "./HomeValue.vue";
+export default {
+  components: {
+    HomeValue,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.home {
+  background: linear-gradient(170deg, hsl(0, 0%, 22%) 0%, hsl(0, 0%, 6%) 30%);
+  padding-bottom: 0;
+}
+.home__title {
+  line-height: 120%;
+}
+.home__search-input::placeholder {
+  color: var(--text-color-light);
+}
+</style>
---
+++ src/components/main/HomeValue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1 class="home__value-number">{{ number }} <span>+</span></h1>
+    <span class="home__value-description">
+      {{ description1 }} <br />
+      {{ description2 }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    number: { type: String, required: true },
+    description1: { type: String, required: true },
+    description2: { type: String, required: true },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
