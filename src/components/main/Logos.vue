86cf7fd0b3b54d23acd050735ce503b7a88c6b14
shoproizoshlo <iamsusunny@gmail.com>
2024-05-08 16:09:09 +0200

    styled logos

--- src/components/main/Logos.vue
+++ src/components/main/Logos.vue
@@ -1,24 +1,21 @@
 <template>
   <section class="logos section">
-    <div class="logos__container wrapper grid">
-      <div class="logos__img">
-        <img src="/src/assets/img/logo1.png" alt="logo" />
-      </div>
-      <div class="logos__img">
-        <img src="/src/assets/img/logo2.png" alt="logo" />
-      </div>
-      <div class="logos__img">
-        <img src="/src/assets/img/logo3.png" alt="logo" />
-      </div>
-      <div class="logos__img">
-        <img src="/src/assets/img/logo4.png" alt="logo" />
-      </div>
+    <div
+      class="pt-8 grid-cols-2 gap-y-12 gap-x-8 justify-items-center wrapper grid"
+    >
+      <LogosImg url="/src/assets/img/logo1.png" />
+      <LogosImg url="/src/assets/img/logo2.png" />
+      <LogosImg url="/src/assets/img/logo3.png" />
+      <LogosImg url="/src/assets/img/logo4.png" />
     </div>
   </section>
 </template>
 
 <script>
-export default {};
+import LogosImg from "./LogosImg.vue";
+export default {
+  components: { LogosImg },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped></style>
---
+++ src/components/main/LogosImg.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="logos__img">
+    <img :src="url" alt="logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.logos__img img {
+  height: 60px;
+  opacity: 0.2;
+  transition: 0.3s;
+}
+
+.logos__img img:hover {
+  opacity: 0.6;
+}
+</style>
