d309589f6cd3c1cb1d2068d4d1e71d3b7ad46785
shoproizoshlo <iamsusunny@gmail.com>
2024-05-15 16:18:42 +0200

    styled popular card

--- src/components/main/Popular.vue
+++ src/components/main/Popular.vue
@@ -7,33 +7,33 @@
         <div class="swiper-wrapper">
           <PopularCard
             url="/src/assets/img/popular1.jpg"
-            price="'66,356'"
-            title="'Garden City Assat'"
-            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+            price="66,356"
+            title="Garden City Assat"
+            description="Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876"
           />
           <PopularCard
             url="/src/assets/img/popular2.jpg"
-            price="'35,159'"
-            title="'Gables Luxurious House'"
-            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+            price="35,159"
+            title="Gables Luxurious House"
+            description="Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876"
           />
           <PopularCard
             url="/src/assets/img/popular3.jpg"
-            price="'75,043'"
-            title="'Garden Orchard City'"
-            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+            price="75,043"
+            title="Garden Orchard City"
+            description="Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876"
           />
           <PopularCard
             url="/src/assets/img/popular4.jpg"
-            price="'62,024'"
-            title="'Luxurious City Garden'"
-            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+            price="62,024"
+            title="Luxurious City Garden"
+            description="Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876"
           />
           <PopularCard
             url="/src/assets/img/popular5.jpg"
-            price="'47,043'"
-            title="'Aliva Private Garden'"
-            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+            price="47,043"
+            title="Aliva Private Garden"
+            description="Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876"
           />
         </div>
 
--- src/components/main/PopularCard.vue
+++ src/components/main/PopularCard.vue
@@ -1,12 +1,16 @@
 <template>
-  <article class="popular__card swiper-slide">
-    <img :src="url" alt="popular projet" class="popular__img" />
+  <article
+    class="my-0 mx-auto pt-2 pb-6 px-2 bg-container-color rounded-2xl popular__card swiper-slide"
+  >
+    <img :src="url" alt="popular projet" class="mb-4 rounded-2xl" />
 
-    <div class="popular__data">
-      <h2 class="popular__price"><span>$</span>{{ price }}</h2>
+    <div class="py-0 ps-4 pe-2">
+      <h2 class="mb-1 text-h2-font-size text-text-color">
+        <span class="text-second-color">$</span>{{ price }}
+      </h2>
 
-      <h3 class="popular__title">{{ title }}</h3>
-      <p class="popular__description">
+      <h3 class="text-h3-font-size mb-3">{{ title }}</h3>
+      <p class="text-small-font-size">
         {{ description }}
       </p>
     </div>
@@ -37,45 +41,10 @@ export default {
 </script>
 
 <style scoped>
-/* my-0 mx-auto pt-2 pb-6 px-2 bg-container-color rounded-2xl */
 .popular__card {
-  margin: 0 auto;
-  padding: 0.5rem 0.5rem 1.5rem;
-  border-radius: 1rem;
-
   width: 290px;
-  background-color: var(--container-color);
   transition: 0.4s;
 }
-/* mb-4 rounded-2xl */
-.popular__img {
-  margin-bottom: 1rem;
-  border-radius: 1rem;
-}
-/* py-0 ps-4 pe-2 */
-.popular__data {
-  padding: 0 1rem 0 0.5rem;
-}
-/* mb-1 text-h2-font-size text-text-color */
-.popular__price {
-  font-size: var(--h2-font-size);
-  color: var(--text-color);
-  margin-bottom: 0.25rem;
-}
-/* text-second-color */
-.popular__price span {
-  color: var(--second-color);
-}
-/* text-h3-font-size mb-3 */
-.popular__title {
-  font-size: var(--h3-font-size);
-  margin-bottom: 0.75rem;
-}
-/* text-small-font-size */
-.popular__description {
-  font-size: var(--small-font-size);
-}
-
 .popular__card:hover {
   box-shadow: 0 12px 16px hsla(228, 66%, 45%, 0.1);
 }
