01e65931f43f394bd86a217bb08a2f3bb3bb79c0
shoproizoshlo <iamsusunny@gmail.com>
2024-05-15 17:08:10 +0200

    fixed Logos component

--- src/components/main/Logos.vue
+++ src/components/main/Logos.vue
@@ -3,19 +3,43 @@
     <div
       class="pt-8 grid-cols-2 gap-y-12 gap-x-8 justify-items-center wrapper grid"
     >
-      <LogosImg url="/src/assets/img/logo1.png" />
+      <!-- <LogosImg url="/src/assets/img/logo1.png" />
       <LogosImg url="/src/assets/img/logo2.png" />
       <LogosImg url="/src/assets/img/logo3.png" />
-      <LogosImg url="/src/assets/img/logo4.png" />
+      <LogosImg url="/src/assets/img/logo4.png" /> -->
+      <LogosImg
+        v-for="(logo, index) in logosUrl"
+        :key="index"
+        :url="logo.url"
+      />
     </div>
   </section>
 </template>
 
 <script>
+import { ref } from "vue";
 import LogosImg from "./LogosImg.vue";
+
 export default {
   components: { LogosImg },
+  setup() {
+    const logosUrl = ref([
+      {
+        url: "/src/assets/img/logo1.png",
+      },
+      {
+        url: "/src/assets/img/logo2.png",
+      },
+      {
+        url: "/src/assets/img/logo3.png",
+      },
+      {
+        url: "/src/assets/img/logo4.png",
+      },
+    ]);
+
+    return { logosUrl };
+  },
 };
 </script>
-
 <style scoped></style>
