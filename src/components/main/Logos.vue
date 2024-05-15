5d2e25d48568c9f99dbaae5047324f16ae506cc3
shoproizoshlo <iamsusunny@gmail.com>
2024-05-15 17:21:40 +0200

    Update Logos.vue

--- src/components/main/Logos.vue
+++ src/components/main/Logos.vue
@@ -3,10 +3,6 @@
     <div
       class="pt-8 grid-cols-2 gap-y-12 gap-x-8 justify-items-center wrapper grid"
     >
-      <!-- <LogosImg url="/src/assets/img/logo1.png" />
-      <LogosImg url="/src/assets/img/logo2.png" />
-      <LogosImg url="/src/assets/img/logo3.png" />
-      <LogosImg url="/src/assets/img/logo4.png" /> -->
       <LogosImg
         v-for="(logo, index) in logosUrl"
         :key="index"
