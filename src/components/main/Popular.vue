ba662f4d3e9228901d43e3ed1c2b1d6183e36132
shoproizoshlo <iamsusunny@gmail.com>
2024-05-15 16:13:32 +0200

    added popular card component

--- src/components/main/Popular.vue
+++ src/components/main/Popular.vue
@@ -5,86 +5,36 @@
       <h2 class="section__title">Popular Residences<span>.</span></h2>
       <div class="popular__container swiper">
         <div class="swiper-wrapper">
-          <article class="popular__card swiper-slide">
-            <img
-              src="/src/assets/img/popular1.jpg"
-              alt="popular projet"
-              class="popular__img"
-            />
-
-            <div class="popular__data">
-              <h2 class="popular__price"><span>$</span>66,356</h2>
-
-              <h3 class="popular__title">Garden City Assat</h3>
-              <p class="popular__description">
-                Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876
-              </p>
-            </div>
-          </article>
-          <article class="popular__card swiper-slide">
-            <img
-              src="/src/assets/img/popular2.jpg"
-              alt="popular projet"
-              class="popular__img"
-            />
-
-            <div class="popular__data">
-              <h2 class="popular__price"><span>$</span>35,159</h2>
-
-              <h3 class="popular__title">Gables Luxurious House</h3>
-              <p class="popular__description">
-                Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876
-              </p>
-            </div>
-          </article>
-          <article class="popular__card swiper-slide">
-            <img
-              src="/src/assets/img/popular3.jpg"
-              alt="popular projet"
-              class="popular__img"
-            />
-
-            <div class="popular__data">
-              <h2 class="popular__price"><span>$</span>75,043</h2>
-
-              <h3 class="popular__title">Garden Orchard City</h3>
-              <p class="popular__description">
-                Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876
-              </p>
-            </div>
-          </article>
-          <article class="popular__card swiper-slide">
-            <img
-              src="/src/assets/img/popular4.jpg"
-              alt="popular projet"
-              class="popular__img"
-            />
-
-            <div class="popular__data">
-              <h2 class="popular__price"><span>$</span>62,024</h2>
-
-              <h3 class="popular__title">Luxurious City Garden</h3>
-              <p class="popular__description">
-                Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876
-              </p>
-            </div>
-          </article>
-          <article class="popular__card swiper-slide">
-            <img
-              src="/src/assets/img/popular5.jpg"
-              alt="popular projet"
-              class="popular__img"
-            />
-
-            <div class="popular__data">
-              <h2 class="popular__price"><span>$</span>47,043</h2>
-
-              <h3 class="popular__title">Aliva Private Garden</h3>
-              <p class="popular__description">
-                Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876
-              </p>
-            </div>
-          </article>
+          <PopularCard
+            url="/src/assets/img/popular1.jpg"
+            price="'66,356'"
+            title="'Garden City Assat'"
+            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+          />
+          <PopularCard
+            url="/src/assets/img/popular2.jpg"
+            price="'35,159'"
+            title="'Gables Luxurious House'"
+            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+          />
+          <PopularCard
+            url="/src/assets/img/popular3.jpg"
+            price="'75,043'"
+            title="'Garden Orchard City'"
+            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+          />
+          <PopularCard
+            url="/src/assets/img/popular4.jpg"
+            price="'62,024'"
+            title="'Luxurious City Garden'"
+            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+          />
+          <PopularCard
+            url="/src/assets/img/popular5.jpg"
+            price="'47,043'"
+            title="'Aliva Private Garden'"
+            description="'Street The Garden City Of Miraflores, Lima - Perú Av. Sol #9876'"
+          />
         </div>
 
         <div class="swiper-button-next">
@@ -99,7 +49,14 @@
 </template>
 
 <script>
-export default {};
+import PopularCard from "./PopularCard.vue";
+export default {
+  components: { PopularCard },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.popular__container {
+  padding: 1rem 0 5rem;
+}
+</style>
---
+++ src/components/main/PopularCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <article class="popular__card swiper-slide">
+    <img :src="url" alt="popular projet" class="popular__img" />
+
+    <div class="popular__data">
+      <h2 class="popular__price"><span>$</span>{{ price }}</h2>
+
+      <h3 class="popular__title">{{ title }}</h3>
+      <p class="popular__description">
+        {{ description }}
+      </p>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* my-0 mx-auto pt-2 pb-6 px-2 bg-container-color rounded-2xl */
+.popular__card {
+  margin: 0 auto;
+  padding: 0.5rem 0.5rem 1.5rem;
+  border-radius: 1rem;
+
+  width: 290px;
+  background-color: var(--container-color);
+  transition: 0.4s;
+}
+/* mb-4 rounded-2xl */
+.popular__img {
+  margin-bottom: 1rem;
+  border-radius: 1rem;
+}
+/* py-0 ps-4 pe-2 */
+.popular__data {
+  padding: 0 1rem 0 0.5rem;
+}
+/* mb-1 text-h2-font-size text-text-color */
+.popular__price {
+  font-size: var(--h2-font-size);
+  color: var(--text-color);
+  margin-bottom: 0.25rem;
+}
+/* text-second-color */
+.popular__price span {
+  color: var(--second-color);
+}
+/* text-h3-font-size mb-3 */
+.popular__title {
+  font-size: var(--h3-font-size);
+  margin-bottom: 0.75rem;
+}
+/* text-small-font-size */
+.popular__description {
+  font-size: var(--small-font-size);
+}
+
+.popular__card:hover {
+  box-shadow: 0 12px 16px hsla(228, 66%, 45%, 0.1);
+}
+</style>
