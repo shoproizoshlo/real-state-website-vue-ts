0c84e380ab62cf00b8588b8164de205092b3da62
shoproizoshlo <iamsusunny@gmail.com>
2024-05-18 08:27:39 +0200

    styled subscribtion section

--- src/components/main/Subscribe.vue
+++ src/components/main/Subscribe.vue
@@ -1,8 +1,10 @@
 <template>
-  <section class="subscribe section">
-    <div class="subscribe__container wrapper">
-      <h1 class="subscribe__title">Get Started with Holux</h1>
-      <p class="subscribe__description">
+  <section class="py-10 px-0 subscribe section">
+    <div
+      class="py-12 px-8 bg-first-color text-center subscribe__container wrapper"
+    >
+      <h1 class="mb-4 text-h2-font-size text-white">Get Started with Holux</h1>
+      <p class="mb-8 text-small-font-size subscribe__description">
         Subscribe and find super attractive price quotes from us, Find your
         residence soon
       </p>
@@ -15,4 +17,15 @@
 export default {};
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.subscribe__container {
+  border-radius: 1.25rem;
+  border: 6px solid var(--first-color-light);
+}
+.subscribe__description {
+  color: hsl(228, 90%, 92%);
+}
+.subscribe__button {
+  border: 2px solid #fff;
+}
+</style>
