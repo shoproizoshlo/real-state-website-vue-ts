f01a65a29f2b1a9935efb7ddd2d509be38293db2
shoproizoshlo <iamsusunny@gmail.com>
2024-05-18 08:55:22 +0200

    Update FooterLinks.vue

--- src/components/footer/FooterLinks.vue
+++ src/components/footer/FooterLinks.vue
@@ -22,75 +22,15 @@
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
   props: {
-    sections: { type: Array, required: true },
+    sections: {
+      type: Array,
+      required: true,
+    },
   },
 };
 </script>
 
-<style scoped>
-.footer__content {
-  grid-template-columns: repeat(2, max-content);
-  gap: 2.5rem 4rem;
-}
-/* text-h3-font-size mb-4 */
-.footer__title {
-  font-size: var(--h3-font-size);
-  margin-bottom: 1rem;
-}
-/* gap-y-2 */
-.footer__links {
-  row-gap: 0.5rem;
-}
-/*  */
-.footer__link {
-  color: var(--text-color);
-  transition: 0.3s;
-}
-.footer__link:hover {
-  color: var(--title-color);
-}
-/* flex gap-x-4 */
-.footer__social {
-  display: flex;
-  column-gap: 1rem;
-}
-/* text-xl text-text-color */
-.footer__social-link {
-  font-size: 1.25rem;
-  color: var(--text-color);
-
-  transition: 0.3s;
-}
-/* hover:text-title-color */
-.footer__social-link:hover {
-  color: var(--title-color);
-}
-/* flex */
-.footer__info,
-.footer__privacy {
-  display: flex;
-}
-/* mt-20 pb-24 flex-col text-center gap-y-6 */
-.footer__info {
-  padding-bottom: 6rem;
-  margin-top: 5.5rem;
-  flex-direction: column;
-  text-align: center;
-  row-gap: 1.5rem;
-}
-/* text-smaller-font-size font-font-medium text-text-color */
-.footer__copy,
-.footer__privacy a {
-  font-size: var(--smaller-font-size);
-  font-weight: var(--font-medium);
-  color: var(--text-color);
-}
-/* justify-center gap-x-5 */
-.footer__privacy {
-  justify-content: center;
-  column-gap: 1.25rem;
-}
-</style>
+<style scoped></style>
