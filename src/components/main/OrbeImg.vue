cecf0ef2b963547970a253209988cd1de0a28acc
shoproizoshlo <iamsusunny@gmail.com>
2024-05-20 09:48:04 +0200

    Update OrbeImg.vue

--- src/components/main/OrbeImg.vue
+++ src/components/main/OrbeImg.vue
@@ -2,7 +2,7 @@
   <div class="value__images">
     <div class="value__orbe"></div>
     <div class="value__img">
-      <img src="/src/assets/img/value.jpg" alt="value" />
+      <img src="/src/assets/img/contact.png" alt="value" />
     </div>
   </div>
 </template>
@@ -17,12 +17,14 @@ export default {};
   display: flex;
   justify-content: center;
 }
+
 .value__orbe {
   width: 266px;
   height: 316px;
   background-color: hsl(228, 24%, 97%);
   border-radius: 135px 135px 16px 16px;
 }
+/* m-auto absolute overflow-hidden inset-0 */
 .value__img {
   width: 250px;
   height: 300px;
@@ -34,29 +36,28 @@ export default {};
   inset: 0;
   margin: auto;
 }
-
 @media screen and (max-width: 320px) {
-  .value__orbe,
-  .contact__orbe {
+  .value__img {
+    width: 220px;
+    height: 260px;
+  }
+  .value__orbe {
     width: 236px;
     height: 280px;
   }
 }
 @media screen and (min-width: 1023px) {
-  .value__orbe,
-  .contact__orbe {
+  .value__orbe {
     width: 501px;
     height: 641px;
     border-radius: 258px 258px 16px 16px;
   }
-  .value__img,
-  .contact__img {
+  .value__img {
     width: 461px;
     height: 601px;
     border-radius: 238px 238px 12px 12px;
   }
-  .value__img img,
-  .contact__img img {
+  .value__img img {
     max-width: inherit;
     width: 490px;
   }
