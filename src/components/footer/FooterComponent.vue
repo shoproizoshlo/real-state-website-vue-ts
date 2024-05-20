d4b57bb5ac2cbf72e2f1e5cc0398054e76cf3c8d
shoproizoshlo <iamsusunny@gmail.com>
2024-05-20 10:54:45 +0200

    styled footer for large devices

--- src/components/footer/FooterComponent.vue
+++ src/components/footer/FooterComponent.vue
@@ -1,40 +1,24 @@
 <template>
   <footer class="footer section">
-    <div class="gap-y-10 wrapper grid">
+    <div class="footer__container wrapper grid">
       <div>
-        <a
-          href="#"
-          class="mb-3 gap-x-1 text-first-color text-h3-font-size font-font-semi-bold inline-flex items-center"
-          >Holux <i class="bx bx-home-alt-2 text-xl"></i
+        <a href="#" class="footer__logo"
+          >Holux <i class="bx bx-home-alt-2"></i
         ></a>
-        <p class="text-small-font-size font-font-medium">
+        <p class="footer__description">
           Our vision is to make all people <br />
           the best place to live for them.
         </p>
       </div>
-      <div class="grid footer__content">
+      <div class="footer__content">
         <FooterLinks :sections="sections" />
       </div>
     </div>
-    <div
-      class="flex mt-20 pb-24 flex-col text-center gap-y-6 footer__info wrapper"
-    >
-      <span
-        class="text-smaller-font-size font-font-medium text-text-color footer__copy"
-      >
-        &#169; Bedimcode. All rigths reserved
-      </span>
-      <div class="justify-center gap-x-5 footer__privacy">
-        <a
-          href="#"
-          class="text-smaller-font-size font-font-medium text-text-color"
-          >Terms & Agreements</a
-        >
-        <a
-          href="#"
-          class="text-smaller-font-size font-font-medium text-text-color"
-          >Privacy Policy</a
-        >
+    <div class="footer__info wrapper">
+      <span class="footer__copy"> &#169; Bedimcode. All rigths reserved </span>
+      <div class="footer__privacy">
+        <a href="#">Terms & Agreements</a>
+        <a href="#">Privacy Policy</a>
       </div>
     </div>
   </footer>
@@ -89,8 +73,88 @@ export default {
 </script>
 
 <style scoped>
+/* gap-y-10 */
+.footer__container {
+  row-gap: 2.5rem;
+}
+/* mb-3 gap-x-1 text-first-color text-h3-font-size font-font-semi-bold inline-flex items-center */
+.footer__logo {
+  color: var(--first-color);
+  font-size: var(--h3-font-size);
+  font-weight: var(--font-semi-bold);
+  display: inline-flex;
+  align-items: center;
+  column-gap: 0.25rem;
+  margin-bottom: 0.75rem;
+}
+/* text-xl */
+.footer__logo i {
+  font-size: 1.25rem;
+}
+/* text-small-font-size font-font-medium */
+.footer__description,
+.footer__link {
+  font-size: var(--small-font-size);
+  font-weight: var(--font-medium);
+}
+
 .footer__content {
+  display: grid;
   grid-template-columns: repeat(2, max-content);
   gap: 2.5rem 4rem;
+}
+/* text-h3-font-size mb-4 */
+
+/* flex */
+.footer__info,
+.footer__privacy {
+  display: flex;
+}
+/* mt-20 pb-24 flex-col text-center gap-y-6 */
+.footer__info {
+  padding-bottom: 6rem;
+  margin-top: 5.5rem;
+  flex-direction: column;
+  text-align: center;
+  row-gap: 1.5rem;
+}
+/* text-smaller-font-size font-font-medium text-text-color */
+.footer__copy,
+.footer__privacy a {
+  font-size: var(--smaller-font-size);
+  font-weight: var(--font-medium);
+  color: var(--text-color);
+}
+/* justify-center gap-x-5 */
+.footer__privacy {
+  justify-content: center;
+  column-gap: 1.25rem;
+}
+
+@media screen and (max-width: 320px) {
+  .footer__content {
+    gap: 2.5rem;
+  }
+}
+@media screen and (min-width: 576px) {
+  .footer__content {
+    grid-template-columns: repeat(3, max-content);
+  }
+}
+@media screen and (min-width: 767px) {
+  .footer__container {
+    grid-template-columns: repeat(2, max-content);
+    justify-content: space-between;
+  }
+}
+@media screen and (min-width: 1023px) {
+  .footer__content {
+    grid-template-columns: repeat(4, max-content);
+  }
+  .footer__info {
+    flex-direction: row;
+    justify-content: space-between;
+    padding-bottom: 2rem;
+  }
 }
 </style>
--- src/components/footer/FooterLinks.vue
+++ src/components/footer/FooterLinks.vue
@@ -1,22 +1,14 @@
 <template>
   <div v-for="section in sections" :key="section.title">
-    <h3 class="text-h3-font-size mb-4 footer__title">{{ section.title }}</h3>
-    <ul v-if="section.links" class="grid gap-y-2 footer__links">
+    <h3 class="footer__title">{{ section.title }}</h3>
+    <ul v-if="section.links" class="footer__links">
       <li v-for="link in section.links" :key="link.text">
-        <a
-          :href="link.href"
-          class="text-small-font-size font-font-medium footer__link"
-          >{{ link.text }}</a
-        >
+        <a :href="link.href" class="footer__link">{{ link.text }}</a>
       </li>
     </ul>
-    <ul v-if="section.socialLinks" class="flex gap-x-4 footer__social">
+    <ul v-if="section.socialLinks" class="footer__social">
       <li v-for="socialLink in section.socialLinks" :key="socialLink.href">
-        <a
-          :href="socialLink.href"
-          target="_blank"
-          class="text-xl text-text-color hover:text-title-color footer__social-link"
-        >
+        <a :href="socialLink.href" target="_blank" class="footer__social-link">
           <i :class="socialLink.icon"></i>
         </a>
       </li>
@@ -36,6 +28,25 @@ export default {
 </script>
 
 <style scoped>
+.footer__description,
+.footer__link {
+  font-size: var(--small-font-size);
+  font-weight: var(--font-medium);
+}
+/* grid */
+.footer__content,
+.footer__links {
+  display: grid;
+}
+.footer__title {
+  font-size: var(--h3-font-size);
+  margin-bottom: 1rem;
+}
+/* gap-y-2 */
+.footer__links {
+  row-gap: 0.5rem;
+}
+/*  */
 .footer__link {
   color: var(--text-color);
   transition: 0.3s;
@@ -43,7 +54,29 @@ export default {
 .footer__link:hover {
   color: var(--title-color);
 }
+/* flex gap-x-4 */
+.footer__social {
+  display: flex;
+  column-gap: 1rem;
+}
+/* text-xl text-text-color */
 .footer__social-link {
+  font-size: 1.25rem;
+  color: var(--text-color);
+
   transition: 0.3s;
+}
+/* hover:text-title-color */
+.footer__social-link:hover {
+  color: var(--title-color);
+}
+
+@media screen and (min-width: 1023px) {
+  .footer__title {
+    margin-bottom: 1.5rem;
+  }
+  .footer__links {
+    row-gap: 1rem;
+  }
 }
 </style>
