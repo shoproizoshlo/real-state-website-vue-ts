cd19f1962611671b714aa6e8a90ca6d4e21055df
shoproizoshlo <iamsusunny@gmail.com>
2024-05-22 22:14:59 +0200

    added tailwind classes to footer section

--- src/components/footer/FooterComponent.vue
+++ src/components/footer/FooterComponent.vue
@@ -1,24 +1,40 @@
 <template>
   <footer class="footer section">
-    <div class="footer__container wrapper grid">
+    <div class="gap-y-10 md:justify-between footer__container wrapper grid">
       <div>
-        <a href="#" class="footer__logo"
-          >Holux <i class="bx bx-home-alt-2"></i
+        <a
+          href="#"
+          class="mb-3 gap-x-1 text-first-color text-h3-font-size font-font-semi-bold inline-flex items-center footer__logo"
+          >Holux <i class="bx bx-home-alt-2 text-xl"></i
         ></a>
-        <p class="footer__description">
+        <p class="text-small-font-size font-font-medium footer__description">
           Our vision is to make all people <br />
           the best place to live for them.
         </p>
       </div>
-      <div class="footer__content">
+      <div class="grid gap-y-10 gap-x-16 footer__content">
         <FooterLinks :sections="sections" />
       </div>
     </div>
-    <div class="footer__info wrapper">
-      <span class="footer__copy"> &#169; Bedimcode. All rigths reserved </span>
-      <div class="footer__privacy">
-        <a href="#">Terms & Agreements</a>
-        <a href="#">Privacy Policy</a>
+    <div
+      class="flex mt-20 pb-24 lg:pb-8 flex-col lg:flex-row text-center lg:justify-between gap-y-6 footer__info wrapper"
+    >
+      <span
+        class="text-smaller-font-size text-text-color gont-font-medium footer__copy"
+      >
+        &#169; Bedimcode. All rigths reserved
+      </span>
+      <div class="flex gap-x-5 justify-center footer__privacy">
+        <a
+          href="#"
+          class="text-smaller-font-size text-text-color gont-font-medium"
+          >Terms & Agreements</a
+        >
+        <a
+          href="#"
+          class="text-smaller-font-size text-text-color gont-font-medium"
+          >Privacy Policy</a
+        >
       </div>
     </div>
   </footer>
@@ -73,59 +89,9 @@ export default {
 </script>
 
 <style scoped>
-/* gap-y-10 */
-.footer__container {
-  row-gap: 2.5rem;
-}
-/* mb-3 gap-x-1 text-first-color text-h3-font-size font-font-semi-bold inline-flex items-center */
-.footer__logo {
-  color: var(--first-color);
-  font-size: var(--h3-font-size);
-  font-weight: var(--font-semi-bold);
-  display: inline-flex;
-  align-items: center;
-  column-gap: 0.25rem;
-  margin-bottom: 0.75rem;
-}
-/* text-xl */
-.footer__logo i {
-  font-size: 1.25rem;
-}
-/* text-small-font-size font-font-medium */
-.footer__description,
-.footer__link {
-  font-size: var(--small-font-size);
-  font-weight: var(--font-medium);
-}
-
 .footer__content {
-  display: grid;
   grid-template-columns: repeat(2, max-content);
-  gap: 2.5rem 4rem;
 }
-/* text-h3-font-size mb-4 */
-
-/* flex */
-.footer__info,
-.footer__privacy {
-  display: flex;
-}
-/* mt-20 pb-24 flex-col text-center gap-y-6 */
-.footer__info {
-  padding-bottom: 6rem;
-  margin-top: 5.5rem;
-  flex-direction: column;
-  text-align: center;
-  row-gap: 1.5rem;
-}
-/* text-smaller-font-size font-font-medium text-text-color */
-.footer__copy,
-.footer__privacy a {
-  font-size: var(--smaller-font-size);
-  font-weight: var(--font-medium);
-  color: var(--text-color);
-}
-/* justify-center gap-x-5 */
 .footer__privacy {
   justify-content: center;
   column-gap: 1.25rem;
@@ -144,17 +110,11 @@ export default {
 @media screen and (min-width: 767px) {
   .footer__container {
     grid-template-columns: repeat(2, max-content);
-    justify-content: space-between;
   }
 }
 @media screen and (min-width: 1023px) {
   .footer__content {
     grid-template-columns: repeat(4, max-content);
-  }
-  .footer__info {
-    flex-direction: row;
-    justify-content: space-between;
-    padding-bottom: 2rem;
   }
 }
 </style>
