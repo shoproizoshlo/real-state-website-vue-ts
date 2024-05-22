2a29573705779a86330cc072b49252fb3612620c
shoproizoshlo <iamsusunny@gmail.com>
2024-05-22 15:46:48 +0200

    added tailwind classes to home section

--- src/components/main/Home.vue
+++ src/components/main/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="home section" id="home">
-    <div class="home__container wrapper grid">
+    <div class="pt-16 gap-y-14 home__container wrapper grid">
       <div class="md:pb-8 lg:pb-16">
         <h1
           class="mb-5 lg:mb-8 text-biggest-font-size lg:text-biggest-font-size-large text-white home__title"
@@ -38,9 +38,11 @@
         </div>
       </div>
 
-      <div class="home__images">
-        <div class="home__orbe"></div>
-        <div class="home__img">
+      <div class="relative flex justify-center home__images">
+        <div class="md:self-end home__orbe"></div>
+        <div
+          class="absolute overflow-hidden inline-flex items-end -bottom-6 lg:-bottom-10 home__img"
+        >
           <img src="/src/assets/img/home.jpg" alt="home" />
         </div>
       </div>
@@ -72,10 +74,6 @@ export default {
   padding-bottom: 0;
   background: linear-gradient(170deg, hsl(0, 0%, 22%) 0%, hsl(0, 0%, 6%) 30%);
 }
-.home__container {
-  padding-top: 4rem;
-  row-gap: 3.5rem;
-}
 
 .home__title {
   line-height: 120%;
@@ -87,11 +85,6 @@ export default {
   color: var(--text-color-light);
 }
 
-.home__images {
-  position: relative;
-  display: flex;
-  justify-content: center;
-}
 .home__orbe {
   width: 265px;
   height: 284px;
@@ -102,13 +95,8 @@ export default {
   );
   border-radius: 135px 135px 0 0;
 }
-.home__img {
-  position: absolute;
-  overflow: hidden;
-  display: inline-flex;
-  align-items: flex-end;
-  bottom: -1.5rem;
 
+.home__img {
   box-shadow: 0 16px 32px hsla(228, 66%, 25%, 0.25);
   width: 250px;
   height: 300px;
@@ -141,9 +129,6 @@ export default {
   .home__container {
     grid-template-columns: repeat(2, 1fr);
   }
-  .home__orbe {
-    align-self: flex-end;
-  }
 }
 @media screen and (min-width: 1023px) {
   .home__orbe {
@@ -151,9 +136,8 @@ export default {
     height: 611px;
     border-radius: 256px 256px 0 0;
   }
-  .home__img {
-    bottom: -2.5rem;
 
+  .home__img {
     width: 472px;
     height: 634px;
     border-radius: 236px 236px 12px 12px;
