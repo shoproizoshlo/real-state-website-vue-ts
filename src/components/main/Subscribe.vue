9d7843bc4befa85225bde64ef884613ae9ec48b3
shoproizoshlo <iamsusunny@gmail.com>
2024-05-22 22:00:51 +0200

    added tailwind classes to subscribe section

--- src/components/main/Subscribe.vue
+++ src/components/main/Subscribe.vue
@@ -1,8 +1,16 @@
 <template>
-  <section class="subscribe section">
-    <div class="subscribe__container wrapper">
-      <h1 class="subscribe__title">Get Started with Holux</h1>
-      <p class="subscribe__description">
+  <section class="subscribe py-10 p-0 section">
+    <div
+      class="py-12 lg:pt-16 pb-16 px-8 md:px-52 lg:px-40 bg-first-color text-center subscribe__container wrapper"
+    >
+      <h1
+        class="mb-4 lg:mb-6 text-h2-font-size lg:text-4xl text-white subscribe__title"
+      >
+        Get Started with Holux
+      </h1>
+      <p
+        class="mb-8 lg:px-32 lg:py-0 text-small-font-size lg:text-normal-font-size-large subscribe__description"
+      >
         Subscribe and find super attractive price quotes from us, Find your
         residence soon
       </p>
@@ -16,38 +24,19 @@ export default {};
 </script>
 
 <style scoped>
-.subscribe {
-  padding: 2.5rem 0;
-}
-/* py-12 px-8 bg-first-color text-center */
 .subscribe__container {
-  background-color: var(--first-color);
-  padding: 3rem 2rem;
-  text-align: center;
-
   border-radius: 1.25rem;
   border: 6px solid var(--first-color-light);
 }
-.subscribe__title {
-  font-size: var(--h2-font-size);
-  color: #fff;
-  margin-bottom: 1rem;
-}
-/* mb-8 text-small-font-size */
 .subscribe__description {
-  font-size: var(--small-font-size);
-  margin-bottom: 2rem;
-
   color: hsl(228, 90%, 92%);
 }
-/* bg-first-color-light text-small-font-size */
 .subscribe__button {
   background: var(--first-color-light);
   font-size: var(--small-font-size);
 
   border: 2px solid #fff;
 }
-/* hover:bg-first-color */
 .subscribe__button:hover {
   background-color: var(--first-color);
 }
@@ -58,25 +47,10 @@ export default {};
   }
 }
 
-@media screen and (min-width: 767px) {
-  .subscribe__container {
-    padding: 3rem 13rem;
-  }
-}
-
 @media screen and (min-width: 1023px) {
   .subscribe__container {
-    padding: 4rem 10rem 4.5rem;
     border-radius: 2rem;
     border: 12px solid var(--first-color-light);
-  }
-  .subscribe__title {
-    font-size: 2.5rem;
-    margin-bottom: 1.5rem;
-  }
-  .subscribe__description {
-    font-size: var(--normal-font-size);
-    padding: 0 8rem;
   }
   .subscribe__button {
     font-size: var(--normal-font-size);
