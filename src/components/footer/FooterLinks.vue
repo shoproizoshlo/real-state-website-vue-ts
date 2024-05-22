5d85dfc011c90c866346d1fa3d18fc5f32f593fd
shoproizoshlo <iamsusunny@gmail.com>
2024-05-22 22:23:39 +0200

    added tailwind classes to footer link section

--- src/components/footer/FooterLinks.vue
+++ src/components/footer/FooterLinks.vue
@@ -1,14 +1,24 @@
 <template>
   <div v-for="section in sections" :key="section.title">
-    <h3 class="footer__title">{{ section.title }}</h3>
-    <ul v-if="section.links" class="footer__links">
+    <h3 class="mb-4 lg:mb-6 text-h3-font-size footer__title">
+      {{ section.title }}
+    </h3>
+    <ul v-if="section.links" class="grid gap-y-2 lg:gap-y-4 footer__links">
       <li v-for="link in section.links" :key="link.text">
-        <a :href="link.href" class="footer__link">{{ link.text }}</a>
+        <a
+          :href="link.href"
+          class="text-small-font-size text-text-color hover:text-title-color font-font-medium duration-300 footer__link"
+          >{{ link.text }}</a
+        >
       </li>
     </ul>
-    <ul v-if="section.socialLinks" class="footer__social">
+    <ul v-if="section.socialLinks" class="flex gap-x-4 footer__social">
       <li v-for="socialLink in section.socialLinks" :key="socialLink.href">
-        <a :href="socialLink.href" target="_blank" class="footer__social-link">
+        <a
+          :href="socialLink.href"
+          target="_blank"
+          class="text-xl text-text-color hover:text-title-color duration-300 footer__social-link"
+        >
           <i :class="socialLink.icon"></i>
         </a>
       </li>
@@ -27,56 +37,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.footer__description,
-.footer__link {
-  font-size: var(--small-font-size);
-  font-weight: var(--font-medium);
-}
-/* grid */
-.footer__content,
-.footer__links {
-  display: grid;
-}
-.footer__title {
-  font-size: var(--h3-font-size);
-  margin-bottom: 1rem;
-}
-/* gap-y-2 */
-.footer__links {
-  row-gap: 0.5rem;
-}
-/*  */
-.footer__link {
-  color: var(--text-color);
-  transition: 0.3s;
-}
-.footer__link:hover {
-  color: var(--title-color);
-}
-/* flex gap-x-4 */
-.footer__social {
-  display: flex;
-  column-gap: 1rem;
-}
-/* text-xl text-text-color */
-.footer__social-link {
-  font-size: 1.25rem;
-  color: var(--text-color);
-
-  transition: 0.3s;
-}
-/* hover:text-title-color */
-.footer__social-link:hover {
-  color: var(--title-color);
-}
-
-@media screen and (min-width: 1023px) {
-  .footer__title {
-    margin-bottom: 1.5rem;
-  }
-  .footer__links {
-    row-gap: 1rem;
-  }
-}
-</style>
+<style scoped></style>
