d054667a5043181ff4fa6130fa00911623c8d54a
shoproizoshlo <iamsusunny@gmail.com>
2024-05-23 22:53:55 +0200

    cleaned some code

--- src/App.vue
+++ src/App.vue
@@ -102,17 +102,6 @@ const scrollActive = () => {
     window.addEventListener("scroll", scrollActive);
   });
 };
-
-// sr.reveal(
-//   ".home__title, .popular__container, .subscribe__container, .footer__container"
-// );
-// sr.reveal(".home__description, .footer__info", { delay: 500 });
-// sr.reveal(".home__search", { delay: 600 });
-// sr.reveal(".home__value", { delay: 700 });
-// sr.reveal(".home__images", { delay: 800, origin: "bottom" });
-// sr.reveal(".logos__img", { interval: 100 });
-// sr.reveal(".value__images, contact__content", { origin: "left" });
-// sr.reveal(".value__content, contact__images", { origin: "right" });
 </script>
 
 <style scoped>
